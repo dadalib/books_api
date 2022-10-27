9ae7f925578d0d58a67cfcbf7edd5e6aa7cc4159
Libioulle <darwin.libioulle@cgi.com>
2022-10-27 14:29:25 +0200

    add import csv

--- import_csv.py
+++ import_csv.py
@@ -1,11 +1,15 @@
 import csv
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session,sessionmaker
 
-# Set up db
-uri = os.getenv("DATABASE_URL")
+
+# Get URL Data Base environement
+uri = os.getenv("DATA_BASE_URL")
+print("uri",uri)
 if uri.startswith('postgres://'):
+    # change postgres to postgresql because it is not support
     uri = uri.replace("postgres://","postgresql://",1)
     print("uri changed",uri)
 
@@ -13,9 +17,10 @@ engine = create_engine(uri)
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    f = open("bookks.csv")
+    f = open("books.csv")
     reader = csv.reader(f)
     for isbn, title, author, year in reader:
+        # Insert data to DB
         db.execute("INSERT INTO books (isbn,title,author,year) VALUES (:isbn, :tittle, :author, :year)",
         {"isbn":isbn,"tittle":title,"author":author,"year":int(year) })
 
